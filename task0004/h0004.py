fd008ac13bac4de35c4ad0cd3dc2afcedb554cd1
cenyoujin <gang_93@126.com>
2016-05-17 07:23:06 +0800

    add

---
+++ task0004/h0004.py
@@ -0,0 +1,9 @@
+import io, re
+
+count = 0
+
+with io.open('text.txt', 'r') as file:
+    for line in file.readlines():
+        list = re.findall("[a-zA-Z]+'*-*[a-zA-Z]*", line)
+        count += len(list)
+print(count)
